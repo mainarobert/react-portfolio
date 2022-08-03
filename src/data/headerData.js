0d40f2000be726b920411e9552c10e9224e78d4d
RobertMaina <robertmaina254@gmail.com>
2022-08-03 22:33:09 +0300

    header data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import myimage from '../assets/png/2021.jpg'
 export const headerData = {
     name: 'Robert Maina',
     title: "Software Developer",
-    desciption:"Hello, my name is Robert Muriuki Maina. I am a software developer who enjoys developing applications using Python and its various frameworks, and also using javaScript and its various frameworks. ",
+    desciption:"Hello, my name is Robert Muriuki Maina. I am a software developer who enjoys developing applications with Python, javaScript and with various libraries like ReactJS and frameworks like Django.",
     image: myimage,
     resumePdf: RobertMainaResume
 }
