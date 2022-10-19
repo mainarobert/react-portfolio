3fb27ad6a0bf8457960022d8ef1d49c6f7af15e9
RobertMaina <robertmaina254@gmail.com>
2022-10-19 15:52:55 +0300

    rename pdf

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import myimage from '../assets/png/2021.jpg'
 export const headerData = {
     name: 'Robert Maina',
     title: "Software Developer",
-    desciption:"Hello, my name is Robert Muriuki Maina. I am a software developer who enjoys developing applications with various programming languages.",
+    desciption:"Hello, my name is Robert Muriuki Maina. I am a software developer who enjoys developing applications with various programming languages and their frameworks and libraries.",
     image: myimage,
     resumePdf: RobertMainaResume
 }
