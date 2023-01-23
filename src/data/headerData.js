ded99b5633ea3567763e84ef22bdf2fe22eed14c
RobertMaina <robertmaina254@gmail.com>
2023-01-23 18:02:22 +0300

    upadate cv

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import myimage from '../assets/png/2021.jpg'
 export const headerData = {
     name: 'Robert Maina',
     title: "Software Developer",
-    desciption:"Hello, my name is Robert Muriuki Maina. I am a software developer who enjoys developing applications with various programming languages with their frameworks and libraries.",
+    desciption:"Hello, my name is Robert Muriuki Maina. I am a software developer who enjoys developing applications websites & web applications using various programming languages along with their frameworks and libraries.",
     image: myimage,
     resumePdf: RobertMainaResume
 }
